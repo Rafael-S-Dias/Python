d7fa21a1f1ef38a190ccd9f1116ce3ba95de5560
Rafael-S-Dias <161033176+Rafael-S-Dias@users.noreply.github.com>
2024-05-03 12:24:44 +0000

    Salve

---
+++ 1.1 Tipagem_Dinâmica.py
@@ -0,0 +1,17 @@
+# Tipagem Dinâmica.
+
+# Variavies.
+nome = "Rafael"
+idade = 22
+peso = 120
+
+# Exibindo Resultados.
+#Simples.
+print(nome)
+print(idade)
+print(peso)
+
+#Formatado.
+print(f"Nome : {nome}")
+print(f"Idade : {idade}")
+print(f"Peso : {peso}")
---
+++ 1.2 Tipagem_Estática.py
@@ -0,0 +1,17 @@
+# Tipagem Estática.
+
+# Variavies.
+nome : str = "Rafael"
+idade : int = 22
+peso : float = 120
+
+# Exibindo Resultados.
+#Simples.
+print(nome)
+print(idade)
+print(peso)
+
+#Formatado.
+print(f"Nome : {nome}")
+print(f"Idade : {idade}")
+print(f"Peso : {peso}")
